cf40a0b8b8a66684330c2ef5413e059056c9e3e8
Jacopo <163671349+Graph2020@users.noreply.github.com>
2026-05-26 13:32:04 +0300

    add flavor-section

--- components/FlavorSlide.tsx
+++ components/FlavorSlide.tsx
@@ -1,9 +1,47 @@
+"use client";
+import { flavorlists } from "@/consts";
+import Image from "next/image";
 import React from "react";
+import { useMediaQuery } from "react-responsive";
 
 export default function FlavorSlide() {
+  const isMobile = useMediaQuery({ query: "(max-width: 640px)" });
+
   return (
     <div className="slider-wrapper">
-      <div className="flavors"></div>
+      <div className="flavors">
+        {flavorlists.map((flavor) => (
+          <div
+            key={flavor.name}
+            className={`relative lg:w-[50vw] w-96 lg:h-[70vh] md:w-[90vw] flex-none md:h-[50vh] h-80 z-30 ${flavor.rotation}`}
+          >
+            <Image
+              width={isMobile ? 500 : 1000}
+              height={isMobile ? 500 : 1000}
+              src={`/images/${flavor.color}-bg.svg`}
+              alt="flavor bevarege"
+            />
+
+            <Image
+              src={`/images/${flavor.color}-drink.webp`}
+              alt={flavor.name}
+              width={isMobile ? 200 : 500}
+              height={isMobile ? 200 : 500}
+              className="drinks"
+            />
+
+            <Image
+              src={`/images/${flavor.color}-elements.webp`}
+              alt=""
+              width={isMobile ? 300 : 500}
+              height={isMobile ? 300 : 500}
+              className="elements"
+            />
+
+            <h1 className="flavor-name">{flavor.name}</h1>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- sections/FlavorSection.tsx
+++ sections/FlavorSection.tsx
@@ -1,8 +1,30 @@
+"use client";
 import FlavorSlide from "@/components/FlavorSlide";
 import FlavorTitle from "@/components/FlavorTitle";
 import React from "react";
 
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { SplitText } from "gsap/dist/SplitText";
+import { ScrollTrigger } from "gsap/src/all";
+import { useRef } from "react";
+
+gsap.registerPlugin(SplitText, ScrollTrigger);
+
 export default function FlavorSection() {
+  useGSAP(() => {
+    gsap.to(".flavor-section", {
+      xPercent: -370,
+      ease: "none",
+      scrollTrigger: {
+        trigger: ".flavor-section",
+        start: "top top",
+        end: "+=1200",
+        scrub: 1,
+        pin: true,
+      },
+    });
+  });
   return (
     <section className="flavor-section">
       <div className="h-full flex lg:flex-row flex-col items-center relative">
